a36c742be1b640f3ec0b074e42cc1134fe6fdc15
Hindia <victorhindia@gmail.com>
2025-03-05 15:56:40 +0300

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import post_detail,post_list,post_share,post_comment
+#from . import views
+app_name='blog'
+urlpatterns=[
+    path('',post_list,name='post_list'),
+    #path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',post_detail,name='post_detail'),
+    path('<int:post_id>/share/',post_share,name='post_share'),
+    path('<int:post_id>/comment/',post_comment, name='post_comment')
+]
