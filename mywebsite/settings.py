a54f76d291601bce3709ca394504dec0b384e394
Hindia <victorhindia@gmail.com>
2025-03-17 01:19:01 +0300

    adding neon progress database

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'mywebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':config('NAME'),
+        'USER':config('USER'),
+        'PASSWORD':config('PASSWORD'),
+        'HOST':config('HOST'),
+        'PORT':config('PORT'),
     }
 }
 
